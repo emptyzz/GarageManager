2c04a794d5b3458a34e2d224443cdbd3eea25a95
emptyzz <radaew2019@Yandex.ru>
2026-02-01 17:08:04 +0500

    add car model

--- GarageManager/Program.cs
+++ GarageManager/Program.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GarageManager.Models;
 
 namespace GarageManager
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            //1
+            var car = new Car();
+            car.Name = "Toyota Crown";
+            car.MileageKm = 464500;
+
+
+            Console.WriteLine(car.Name);
+            Console.WriteLine(car.MileageKm);
         }
     }
 }
