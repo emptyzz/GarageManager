ea604640ad24a3aaa59f05f6240ac7376db87ce1
emptyzz <radaew2019@Yandex.ru>
2026-02-02 02:18:40 +0500

    add GarageService with car storage

---
+++ GarageManager/Models/Services/GarageService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarageManager.Models.Services
+{
+    internal class GarageService
+    {
+        private List<Car> _cars = new List<Car>();
+
+        public void AddCar(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public IReadOnlyList<Car> GetCars()
+        {
+            return _cars;
+        }
+    }
+}
--- GarageManager/Program.cs
+++ GarageManager/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using GarageManager.Models;
+using GarageManager.Models.Services;
 
 namespace GarageManager
 {
@@ -11,7 +12,26 @@ namespace GarageManager
     {
         static void Main()
         {
-            
+            var garage = new GarageService();
+
+            var car1 = new Car();
+            car1.Name = "Toyota Crown";
+            car1.MileageKm = 245000;
+
+            var car2 = new Car();
+            car2.Name = "Mark II";
+            car2.MileageKm = 180000;
+
+            garage.AddCar(car1);
+            garage.AddCar(car2);
+
+            var cars = garage.GetCars();
+
+            foreach (var c in cars)
+            {
+                Console.WriteLine(c.Name + " - " + c.MileageKm + " km");
+            }
+
         }
     }
 }
