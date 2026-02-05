3e7bdf56ec04af9e6f0a9efcf64c4a07004f478c
emptyzz <radaew2019@Yandex.ru>
2026-02-05 08:38:33 +0500

    feat(models): add GarageData container for persistence

---
+++ GarageManager/Models/GarageData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarageManager.Models
+{
+    internal class GarageData
+    {
+        public List<Car> Cars { get; set; } = new List<Car>();
+        public List<ServiceRecord> Records { get; set; } = new List<ServiceRecord>();
+    }
+}
