9246f9f5031984e8f8f552b5cffe272780650e00
emptyzz <radaew2019@Yandex.ru>
2026-02-06 00:31:08 +0500

    feat(services): add JSON persistence service

--- GarageManager/Services/PersistenceService.cs
+++ GarageManager/Services/PersistenceService.cs
@@ -1,12 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+using GarageManager.Models;
 
 namespace GarageManager.Services
 {
     internal class PersistenceService
     {
+        private readonly string _filePath;
+
+        public PersistenceService(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public void Save(GarageData data)
+        {
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true
+            };
+
+            string json = JsonSerializer.Serialize(data, options);
+            File.WriteAllText(_filePath, json);
+        }
+
+        public GarageData Load()
+        {
+            if (!File.Exists(_filePath))
+                return new GarageData();
+
+            try
+            {
+                string json = File.ReadAllText(_filePath);
+                var data = JsonSerializer.Deserialize<GarageData>(json);
+                return data ?? new GarageData();
+            }
+            catch
+            {
+                return new GarageData();
+            }
+        }
     }
 }
