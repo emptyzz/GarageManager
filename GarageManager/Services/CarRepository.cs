45ffb7af25dc9dc54aa49dfebf9b469ab877aeaf
emptyzz <radaew2019@Yandex.ru>
2026-02-10 22:05:43 +0500

    feat(db): read cars from SQLite

--- GarageManager/Services/CarRepository.cs
+++ GarageManager/Services/CarRepository.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Data.Sqlite;
+﻿using GarageManager.Models;
+using Microsoft.Data.Sqlite;
 
 namespace GarageManager.Services
 {
@@ -19,6 +20,30 @@ namespace GarageManager.Services
             command.Parameters.AddWithValue("@mileage", mileage);
 
             command.ExecuteNonQuery();
+        }
+
+        public static List<Car> GetCars()
+        {
+            var cars = new List<Car>();
+
+            var connectionString = "Data Source=garage.db";
+            using var connection = new SqliteConnection(connectionString);
+            connection.Open();
+            using var command = connection.CreateCommand();
+            command.CommandText = "SELECT Id, Name, MileageKm FROM Cars ORDER BY Id;";
+            
+            using var reader = command.ExecuteReader();
+            while (reader.Read()) 
+            {
+                int id = reader.GetInt32(0);
+                string name = reader.GetString(1);
+                int mileage = reader.GetInt32(2);
+
+                var car = new Car() { Id = id, Name = name, MileageKm = mileage };
+
+                cars.Add(car);
+            }
+            return cars;
         }
     }
 }
