b70225040a5e4153252f6cbc3a110be6b292c3b5
emptyzz <radaew2019@Yandex.ru>
2026-05-19 12:49:22 +0500

    feat(api): add CarsController with hardcoded car list

---
+++ GarageManager.Api/CarsController/CarsController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using GarageManager.Api.Models;
+
+namespace GarageManager.Api.Controllers;
+
+[ApiController]
+[Route("api/cars")]
+
+public class CarsController : ControllerBase
+{
+    private static readonly List<Car> _cars = new()
+    {
+        new Car { Id = 1, Name = "Toyota Crown", MileageKm = 999 },
+        new Car { Id = 2, Name = "Toyota Mark II GX81", MileageKm = 320000 },
+        new Car { Id = 3, Name = "BMW E39 530i", MileageKm = 250000 }
+    };
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(_cars);
+    }
+}
