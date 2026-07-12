2145f0636dd9b87a194013153e728fcbcc8178f7
emptyzz <radaew2019@Yandex.ru>
2026-07-12 19:41:54 +0500

    test(api): add integration test for GET cars

---
+++ GarageManager.Tests/CarsControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace GarageManager.Tests
+{
+    public class CarsControllerTests : IClassFixture<CustomWebApplicationFactory>
+    {
+        private readonly CustomWebApplicationFactory _factory;
+
+        public CarsControllerTests(CustomWebApplicationFactory factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task GetCars_ReturnsSuccessStatusCode()
+        {
+            var client = _factory.CreateClient();
+            var response = await client.GetAsync("/api/cars");
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
